80de8760e047d96253e79d7aa6def7957ef0af2c
김도언 <vov06144@naver.com>
2023-01-29 16:40:40 +0900

    Upload Axios exam

--- axios-exam/axios-exam/src/AxiosApi.js
+++ axios-exam/axios-exam/src/AxiosApi.js
@@ -9,6 +9,36 @@ function AxiosApi() {
 
   // 통신 메서드
   function searchApi() {
-    const url = 
+    const url = "https://jsonplaceholder.typicode.com/photos";
+    axios.get(url)
+    .then(function(response) {
+      setPhotos(response.data);
+      console.log('성공');
+    })
+    .catch(function(error) {
+      console.log('실패');
+    })
   }
+
+  // 조회 데이터 존재할 경우
+  if (photos.length > 0) {
+    return (
+      photos.map(photo => (
+        (photo.id < 10) ? (
+          <div key={photo.id}>
+            <img src={photo.thumbnailUrl} alt="img" />
+            <p>title : {photo.title}</p>
+          </div>
+        )
+        : null
+      ))
+    );
+  } else { //조회 데이터 없을 경우
+    return (
+      <div>
+        <button onClick={searchApi}>불러오기</button>
+      </div>
+    )}
 }
+
+export default AxiosApi;
